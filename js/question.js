5f79fbe98d3115b2bd6fda93a743df25ee779771
nandita <nanditap580@gmail.com>
2023-04-11 23:46:28 +0530

    new file:   .vscode/settings.json 	new file:   README.md 	new file:   img/Question.svg 	new file:   img/mongodbcloud-1.jpg 	new file:   img/winner.svg 	new file:   index.html 	new file:   js/question.js 	new file:   js/script.js 	new file:   style.css

---
+++ js/question.js
@@ -0,0 +1,59 @@
+let questions = [
+  {
+      numb:1,
+      question:'Which of the following language is MongoDB written in?',
+      answer:'All of the mentioned',
+      options:[
+          "Javascript",
+          "C",
+          "C++",
+          "All of the mentioned"
+      ]
+  },
+  {
+      numb: 2,
+      question: "Which of the following format is supported by MongoDB?",
+      answer: "BSON",
+      options: [
+        "SQL",
+        "XML",
+        "BSON",
+        "All of the mentioned"
+      ]
+    },
+    {
+      numb: 3,
+      question: "MongoDB stores all documents in _____________?",
+      answer: "collections",
+      options: [
+        "tables",
+        "collections",
+        "rows",
+        "all of the mentioned"
+      ]
+    },
+  
+    {
+      numb: 4,
+      question: "MongoDB process collection of documents using _________ operations.",
+      answer: "Map-reduce",
+      options: [
+        "Hbase",
+        "Hive",
+        "Map-reduce",
+        "all of the mentioned"
+      ]
+    },
+
+    {
+      numb: 5,
+      question: "Capped collections provide __________ management of inserted documents in MongoDB.",
+      answer: "FIFO",
+      options: [
+        "FIFO",
+        "LIFO",
+        "LRU",
+        "MRU"
+      ]
+    },
+]
